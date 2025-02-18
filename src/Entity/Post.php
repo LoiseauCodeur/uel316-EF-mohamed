c974ba0cb26e226871c1cc3e9ebded74af0385c0
Cheuvalux Tzu <aldanmarki77@gmail.com>
2025-02-18 19:36:27 +0100

    done

---
+++ src/Entity/Post.php
@@ -0,0 +1,96 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\PostRepository;
+use Doctrine\DBAL\Types\Types;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: PostRepository::class)]
+class Post
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $title = null;
+
+    #[ORM\Column(type: Types::TEXT, nullable: true)]
+    private ?string $description = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $author = null;
+
+    #[ORM\Column(nullable: true)]
+    private ?int $nbLikes = null;
+
+    #[ORM\Column(nullable: true)]
+    private ?int $nbDislike = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getTitle(): ?string
+    {
+        return $this->title;
+    }
+
+    public function setTitle(string $title): static
+    {
+        $this->title = $title;
+
+        return $this;
+    }
+
+    public function getDescription(): ?string
+    {
+        return $this->description;
+    }
+
+    public function setDescription(?string $description): static
+    {
+        $this->description = $description;
+
+        return $this;
+    }
+
+    public function getAuthor(): ?string
+    {
+        return $this->author;
+    }
+
+    public function setAuthor(string $author): static
+    {
+        $this->author = $author;
+
+        return $this;
+    }
+
+    public function getNbLikes(): ?int
+    {
+        return $this->nbLikes;
+    }
+
+    public function setNbLikes(?int $nbLikes): static
+    {
+        $this->nbLikes = $nbLikes;
+
+        return $this;
+    }
+
+    public function getNbDislike(): ?int
+    {
+        return $this->nbDislike;
+    }
+
+    public function setNbDislike(?int $nbDislike): static
+    {
+        $this->nbDislike = $nbDislike;
+
+        return $this;
+    }
+}
